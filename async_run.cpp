5c66d27ddc97bd9aff8dae2175fef972411cd7df
Yu-Tsung Tai (戴淯琮) <yuttai@indiana.edu>
2015-11-15 01:47:16 -0500

    Adding commens in Parallel.nb; rename main to parallel in async_run.cpp
    Add comments in Parallel.nb
    Rename main to int parallel(const int numberJobs, const int time, const
    bool echo, const bool test); in async_run.cpp

--- async_run.cpp
+++ async_run.cpp
@@ -8,38 +8,49 @@
 
 using namespace std;
 
-int loop(int i){
-	cout << i << " loop run!\n";
-	for(int i = 0; i < 2000000000; i++);
-	cout << i << " loop end!\n";
+int loop(const int loopNum, const int time){
+	if(loopNum >= 0) cout << loopNum << " loop run!\n";
+	for(int j = 0; j < time; j++);
+	if(loopNum >= 0) cout << loopNum << " loop end!\n";
 	return 0;
 }
 
-int main (int argc, char * argv[]){
-	cout << "main run!\n";
-  future<int> newsockfd[4];
-  for(int i = 0; i < 4; i++){
+int parallel(const int numberJobs, const int time, const bool echo, const bool test){
+	cout << "parallel run with " << numberJobs << " of jobs. \n";
+  future<int> jobs[numberJobs];
+  for(int i = 0; i < numberJobs; i++){
 		try{
-			newsockfd[i] = async(launch::async , loop, i);
+			jobs[i] = async(launch::async , loop, (echo ? i : -1), time);
 		}
 		catch(exception& rEx){
 			cout << endl << typeid(rEx).name() << " caught in Active: " << rEx.what();
 			exit(1);
 		}
   }
-  //main loop
-  cout << "testing async result!\n";
-  int i = 0;
-  while(i < 4){
-    for(i = 0; i < 4; i++){
-    	if(newsockfd[i].wait_for(chrono::seconds(0)) != future_status::ready) break;
-    }
-    this_thread::sleep_for(chrono::nanoseconds(1));
+
+  if(test){
+		//main loop
+		cout << "testing async result!\n";
+		int i = 0;
+		while(i < numberJobs){
+			for(i = 0; i < numberJobs; i++){
+				if(jobs[i].wait_for(chrono::seconds(0)) != future_status::ready) break;
+			}
+			this_thread::sleep_for(chrono::nanoseconds(1));
+		}
   }
 
+  cout << "When finish\n";
   int total = 0;
-  for(int i = 0; i < 4; i++){
-  	total += newsockfd[i].get();
+  for(int i = 0; i < numberJobs; i++){
+  	total += jobs[i].get();
   }
+  cout << total << "\n";
   return total;
+}
+
+int main (int argc, char * argv[]){
+	parallel(4, 2000000000, true, false);
+	parallel(20000, 100000, false, false);
+	return 0;
 }
